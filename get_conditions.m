22ee0c5776752804cb4cedcb1af465bce39c0fcd
Eshed Margalit <eshed.margalit@gmail.com>
2016-10-22 09:12:05 -0500

    Fixed bug in conditions list

--- get_conditions.m
+++ get_conditions.m
@@ -1,7 +1,7 @@
 function conditions = get_conditions(experiment)
 	switch experiment
 		case 'floc'
-			conditions={'characters','bodyparts','faces','places','objects','word','number','body','limb','adult','child','corridor','house','car','instrument'};
+			conditions={'characters','bodies','faces','places','objects','word','number','body','limb','adult','child','corridor','house','car','instrument'};
 		otherwise
 			error('Experiment not recognized. Add it to the list!');
 	end
